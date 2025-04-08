0cc132e2fc5808bf55fb8f5e64f76d776476a1e4
Mruu4 <a55957b@gmail.com>
2025-04-08 20:45:05 +0300

    commit #3
    In the NV line 4, the degree Celsius
    was changed from 22 to 20.

--- src/LocalWeather.java
+++ src/LocalWeather.java
@@ -1,6 +1,6 @@
 public class LocalWeather implements WeatherReporter {
     @Override
     public String getReport() {
-        return "Local: Sunny, 22°C";
+        return "Local: Sunny, 20°C";
     }
 }
